07cb7c002e6a9f4dfb9751114ea8e85e4e0cbb4b
elBibi <my.ajt.knight@gmail.com>
2024-06-07 13:18:30 -0400

    Working version, with one test

--- src/main/java/com/example/springboot_helloworld/HelloWorldController.java
+++ src/main/java/com/example/springboot_helloworld/HelloWorldController.java
@@ -1,8 +1,14 @@
 package com.example.springboot_helloworld;
 
-import org.springframework.
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-
+@RestController
 public class HelloWorldController {
+    @GetMapping("hello")
+    public String hello(){
+        System.out.println("Hello World from Michel Knight 0");
+        return  "Hello World from Michel Knight";
+    }
 }
---
+++ src/test/java/com/example/springboot_helloworld/HelloWorldControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.springboot_helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloWorldController.class)
+public class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World from Michel Knight"));
+    }
+}
